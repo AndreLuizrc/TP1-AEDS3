6ae8b19a24d0ca4ad0b164daa7f478ed0403c232
unknown <joao.diasest@gmail.com>
2024-09-15 20:29:04 -0300

    terminando implementacao hash extensivel

--- Arquivos/Arquivo.java
+++ Arquivos/Arquivo.java
@@ -12,9 +12,9 @@ public class Arquivo<T extends Registro> {
     RandomAccessFile arquivo;
     String nomeArquivo;
     Constructor<T> construtor;
-    HashExtensivel<ParEnderecoId> pie;
+    HashExtensivel<ParEnderecoId> indiceDireto;
 
-    public Arquivo(String na, Constructor<T> c) throws IOException {
+    public Arquivo(String na, Constructor<T> c) throws Exception {
         File d = new File(".\\dados");
         if(!d.exists())
             d.mkdir();
@@ -26,118 +26,120 @@ public class Arquivo<T extends Registro> {
             // inicializa o arquivo, criando seu cabecalho
             arquivo.writeInt(0);
         }
+        indiceDireto = new HashExtensivel<>(
+                ParEnderecoId.class.getConstructor(),
+                10,
+                ".\\dados\\diretorio.hash.db",
+                ".\\dados\\cestos.hash.db");
+
     }
 
     public int create(T obj) throws Exception {
         arquivo.seek(0);
-        int proximoID = arquivo.readInt()+1;
+        int proximoID = arquivo.readInt() + 1;
         arquivo.seek(0);
         arquivo.writeInt(proximoID);
         obj.setId(proximoID);
-        arquivo.seek(arquivo.length());
+
+        long endereco = arquivo.length();
 
         byte[] b = obj.toByteArray();
-        arquivo.writeByte(' ');
-        arquivo.writeShort(b.length);
+        arquivo.writeByte(' '); // lapide
+        arquivo.writeShort(b.length); // tamanho do registro
         arquivo.write(b);
-        
+
+        indiceDireto.create(new ParEnderecoId(proximoID, endereco));
+
         return obj.getId();
     }
-    
+
     public T read(int id) throws Exception {
         T obj;
         short tam;
         byte[] b;
         byte lapide;
-        arquivo.seek(TAM_CABECALHO);
-        while(arquivo.getFilePointer()<arquivo.length()) {
-            obj = construtor.newInstance();
-            lapide = arquivo.readByte();
-            tam = arquivo.readShort();
-            b = new byte[tam];
-            arquivo.read(b);
+        ParEnderecoId par = indiceDireto.read(id);
 
-            if(lapide==' ') {
-                obj.fromByteArray(b);
-                if(obj.getId()==id)
-                    return obj;
+        if (par != null) {
+            // Verificar se o endereço está dentro do comprimento do arquivo
+            if (par.getEndereco() < arquivo.length()) {
+                arquivo.seek(par.getEndereco());
+                obj = construtor.newInstance();
+                lapide = arquivo.readByte();
+                if (lapide == ' ') {
+                    tam = arquivo.readShort();
+                    b = new byte[tam];
+
+                    if (arquivo.getFilePointer() + tam <= arquivo.length()) {
+                        arquivo.read(b);
+                        obj.fromByteArray(b);
+                        if (obj.getId() == id)
+                            return obj;
+                    } else {
+                        System.err.println("Tamanho do registro excede os limites do arquivo.");
+                    }
+                }
+            } else {
+                System.err.println("Endereço inválido: " + par.getEndereco());
             }
         }
         return null;
     }
 
     public boolean delete(int id) throws Exception {
-        T obj;
-        short tam;
-        byte[] b;
-        byte lapide;
-        Long endereco;
-        arquivo.seek(TAM_CABECALHO);
-        while(arquivo.getFilePointer()<arquivo.length()) {
-            obj = construtor.newInstance();
-            endereco = arquivo.getFilePointer();
-            lapide = arquivo.readByte();
-            tam = arquivo.readShort();
-            b = new byte[tam];
-            arquivo.read(b);
+       ParEnderecoId par = indiceDireto.read(id);
+       if(par == null)
+           return false;
 
-            if(lapide==' ') {
-                obj.fromByteArray(b);
-                if(obj.getId()==id) {
-                    arquivo.seek(endereco);
-                    arquivo.write('*');
-                    return true;
-                }
-            }
-        }
+       arquivo.seek(par.getEndereco());
+       byte lapide = arquivo.readByte();
+       if (lapide == ' '){
+           arquivo.seek(par.getEndereco());
+           arquivo.writeByte('*');
+           indiceDireto.delete(id);
+           return true;
+       }
+
         return false;
     }
 
     public boolean update(T novoObj) throws Exception {
-        T obj;
-        short tam;
-        byte[] b;
-        byte lapide;
-        Long endereco;
-        arquivo.seek(TAM_CABECALHO);
-        while(arquivo.getFilePointer()<arquivo.length()) {
-            obj = construtor.newInstance();
-            endereco = arquivo.getFilePointer();
-            lapide = arquivo.readByte();
-            tam = arquivo.readShort();
-            b = new byte[tam];
-            arquivo.read(b);
+        ParEnderecoId par = indiceDireto.read(novoObj.getId());
+        if(par == null)
+            return false;
 
-            if(lapide==' ') {
-                obj.fromByteArray(b);
-                if(obj.getId()==novoObj.getId()) {
+        arquivo.seek(par.getEndereco());
+        byte lapide = arquivo.readByte();
+        short tam = arquivo.readShort();
+        byte[] b = new byte[tam];
+        arquivo.read(b);
 
-                    byte[] b2 = novoObj.toByteArray();
-                    short tam2 = (short)b2.length;
+        if(lapide == ' '){
+            byte[] b2 = novoObj.toByteArray();
+            short tam2 = (short) b2.length;
 
-                    // sobrescreve o registro
-                    if(tam2 <= tam) {
-                        arquivo.seek(endereco+3);
-                        arquivo.write(b2);
-                    }
+            if(tam2 <= tam){
+                arquivo.seek(par.getEndereco() + 3);
+                arquivo.write(b2);
+            } else {
+                arquivo.seek(par.getEndereco());
+                arquivo.writeByte('*');
 
-                    // move o novo registro para o fim
-                    else {
-                        arquivo.seek(endereco);
-                        arquivo.write('*');
-                        arquivo.seek(arquivo.length());
-                        arquivo.writeByte(' ');
-                        arquivo.writeShort(tam2);
-                        arquivo.write(b2);
-                    }
-                    return true;
-                }
+                long novoEndereco = arquivo.length();
+                arquivo.seek(novoEndereco);
+                arquivo.writeByte(' ');
+                arquivo.writeShort(tam2);
+                arquivo.write(b2);
+
+                par.setEndereco(novoEndereco);
+                indiceDireto.update(par);
             }
+            return true;
         }
         return false;
     }
 
-    
+
 
     public void close() throws IOException {
         arquivo.close();
--- IO.java
+++ IO.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.time.LocalDate;
-
 import Arquivos.Arquivo;
 import Objetos.Tarefas;
 import Utils.Status;
@@ -14,38 +13,50 @@ public class IO {
         Tarefas t3 = new Tarefas(3, "Finalizar o projeto", LocalDate.of(2023, 9, 3));
 
         try {
-            File f = new File(".\\dados\\tarefas.db");
-            f.delete();
+            ".\\dados"
+            // Deleta os arquivos de dados e hash para iniciar com um ambiente limpo
+            new File(".\\dados\\tarefas.db").delete();
+            new File(".\\dados\\diretorio.hash.db").delete();
+            new File(".\\dados\\cestos.hash.db").delete();
 
+            // Inicializa o arquivo utilizando a classe Tarefas
             arqTarefas = new Arquivo<>("tarefas.db", Tarefas.class.getConstructor());
 
+            // Criar tarefas no arquivo
             arqTarefas.create(t1);
             arqTarefas.create(t2);
             arqTarefas.create(t3);
 
-            Tarefas t = arqTarefas.read(3);
-            if (t != null)
-                System.out.println(t);
-            else
+            // Testa a leitura da tarefa 1
+            Tarefas t = arqTarefas.read(1);
+            if (t != null) {
+                System.out.println("Tarefa encontrada: " + t);
+            } else {
                 System.out.println("\nTarefa não encontrada!");
+            }
 
-            t = arqTarefas.read(1);
-            if (t != null)
-                System.out.println(t);
-            else
+            // Testa a leitura da tarefa 3
+            t = arqTarefas.read(3);
+            if (t != null) {
+                System.out.println("Tarefa encontrada: " + t);
+            } else {
                 System.out.println("\nTarefa não encontrada!");
+            }
 
-            t2.setId(2);
+            // Atualiza a tarefa 2 para concluída
             t2.setStatus(Status.CONCLUIDO);
             t2.setDoneAt(LocalDate.now());
             arqTarefas.update(t2);
 
+            // Testa a leitura da tarefa 2 atualizada
             t = arqTarefas.read(2);
-            if (t != null)
-                System.out.println(t);
-            else
+            if (t != null) {
+                System.out.println("Tarefa atualizada: " + t);
+            } else {
                 System.out.println("\nTarefa não encontrada!");
+            }
 
+            // Fecha o arquivo de tarefas
             arqTarefas.close();
 
         } catch (Exception e) {
