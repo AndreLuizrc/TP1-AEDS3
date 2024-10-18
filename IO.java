51c38ef36a0f191446a0c0483ff33091dbdaa630
dominicsg <douglasnicsg@hotmail.com>
2024-10-18 00:36:14 -0300

    Menu IO adicionado, provavelmente com erros.

--- IO.java
+++ IO.java
@@ -3,64 +3,49 @@ import java.time.LocalDate;
 //import Arquivos.Arquivo;
 import Arquivos.ArquivoCategoria;
 import Objetos.Tarefas;
+import Utils.MenuCategorias;
 import Utils.Status;
+import java.util.Scanner;
 
 public class IO {
 
     public static void main(String[] args) {
-        ArquivoCategoria arqCategorias;
-
-        Tarefas t1 = new Tarefas(1, "Comprar mantimentos", LocalDate.of(2023, 9, 1));
-        Tarefas t2 = new Tarefas(2, "Estudar para o exame", LocalDate.of(2023, 8, 25));
-        Tarefas t3 = new Tarefas(3, "Estudar para a prova de Aeds 3", LocalDate.of(2023, 9, 3));
+        Scanner console;
 
         try {
-            // Deleta os arquivos de dados e hash para iniciar com um ambiente limpo
-            new File(".\\dados\\tarefas.db").delete();
-            new File(".\\dados\\tarefas.hash_d.db").delete();
-            new File(".\\dados\\tarefas.hash_c.db").delete();
+            console = new Scanner(System.in);
+            int opcao;
+            do {
 
-            // Inicializa o arquivo utilizando a classe Tarefas
-            arqCategorias = new ArquivoCategoria("tarefas", Tarefas.class.getConstructor());
+                System.out.println("AEDsIII");
+                System.out.println("-------");
+                System.out.println("\n> Início");
+                System.out.println("1 - Categorias");
+                System.out.println("2 - Clientes");
+                System.out.println("0 - Sair");
 
-            // Criar tarefas no arquivo
-            arqCategorias.create(t1);
-            arqCategorias.create(t2);
-            arqCategorias.create(t3);
+                System.out.print("Opção: ");
+                try {
+                    opcao = Integer.valueOf(console.nextLine());
+                } catch (NumberFormatException e) {
+                    opcao = -1;
+                }
 
-            // Testa a leitura da tarefa 1
-            Tarefas t = arqCategorias.read(1);
-            if (t != null) {
-                System.out.println("Tarefa encontrada: " + t + "\n");
-            } else {
-                System.out.println("\nTarefa não encontrada!");
-            }
+                switch (opcao) {
+                    case 1:
+                        (new MenuCategorias()).menu();
+                        break;
+                    case 2:
+                        // (new MenuClientes()).menu();
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Opção inválida!");
+                        break;
+                }
 
-            // Testa a leitura da tarefa 3
-            t = arqCategorias.read(3);
-            if (t != null) {
-                System.out.println("Tarefa encontrada: " + t + "\n");
-            } else {
-                System.out.println("\nTarefa não encontrada!");
-            }
-
-            // sempre setar a data de conclusão quando alterar o status da tarefa para
-            // concluído
-            t3.setStatus(Status.CONCLUIDO);
-            t3.setDoneAt(LocalDate.of(2023, 9, 5));
-
-            t3.setPriority((byte) 3);
-
-            arqCategorias.update(t3);
-
-            t = arqCategorias.read(3);
-            if (t != null) {
-                System.out.println("Tarefa encontrada: " + t + "\n");
-            } else {
-                System.out.println("\nTarefa não encontrada!");
-            }
-
-            arqCategorias.close();
+            } while (opcao != 0);
 
         } catch (Exception e) {
             e.printStackTrace();
