9f5db02023d0a203b86eb7e57d440dcc59ba0320
dominicsg <douglasnicsg@hotmail.com>
2024-10-17 23:01:53 -0300

    ArquivoCategoria na main e gitignore na raiz

--- Arquivos/ArquivoCategoria.java
+++ Arquivos/ArquivoCategoria.java
@@ -2,7 +2,7 @@ package Arquivos;
 
 import Objetos.Tarefas;
 import java.lang.reflect.Constructor;
-import Objetos.Tarefas;
+//import Objetos.Tarefas;
 
 public class ArquivoCategoria extends Arquivo<Tarefas> {
     public ArquivoCategoria(String na, Constructor<Tarefas> c) throws Exception {
--- IO.java
+++ IO.java
@@ -1,13 +1,15 @@
 import java.io.File;
 import java.time.LocalDate;
-import Arquivos.Arquivo;
+//import Arquivos.Arquivo;
+import Arquivos.ArquivoCategoria;
 import Objetos.Tarefas;
 import Utils.Status;
 
 public class IO {
 
     public static void main(String[] args) {
-        Arquivo<Tarefas> arqTarefas;
+        ArquivoCategoria arqCategorias;
+
         Tarefas t1 = new Tarefas(1, "Comprar mantimentos", LocalDate.of(2023, 9, 1));
         Tarefas t2 = new Tarefas(2, "Estudar para o exame", LocalDate.of(2023, 8, 25));
         Tarefas t3 = new Tarefas(3, "Estudar para a prova de Aeds 3", LocalDate.of(2023, 9, 3));
@@ -17,17 +19,17 @@ public class IO {
             new File(".\\dados\\tarefas.db").delete();
             new File(".\\dados\\tarefas.hash_d.db").delete();
             new File(".\\dados\\tarefas.hash_c.db").delete();
-            
+
             // Inicializa o arquivo utilizando a classe Tarefas
-            arqTarefas = new Arquivo<>("tarefas", Tarefas.class.getConstructor());
+            arqCategorias = new ArquivoCategoria("tarefas", Tarefas.class.getConstructor());
 
             // Criar tarefas no arquivo
-            arqTarefas.create(t1);
-            arqTarefas.create(t2);
-            arqTarefas.create(t3);
+            arqCategorias.create(t1);
+            arqCategorias.create(t2);
+            arqCategorias.create(t3);
 
             // Testa a leitura da tarefa 1
-            Tarefas t = arqTarefas.read(1);
+            Tarefas t = arqCategorias.read(1);
             if (t != null) {
                 System.out.println("Tarefa encontrada: " + t + "\n");
             } else {
@@ -35,32 +37,30 @@ public class IO {
             }
 
             // Testa a leitura da tarefa 3
-            t = arqTarefas.read(3);
+            t = arqCategorias.read(3);
             if (t != null) {
                 System.out.println("Tarefa encontrada: " + t + "\n");
             } else {
                 System.out.println("\nTarefa não encontrada!");
             }
 
-            //sempre setar a data de conclusão quando alterar o status da tarefa para concluído
+            // sempre setar a data de conclusão quando alterar o status da tarefa para
+            // concluído
             t3.setStatus(Status.CONCLUIDO);
             t3.setDoneAt(LocalDate.of(2023, 9, 5));
 
             t3.setPriority((byte) 3);
-            
 
-            arqTarefas.update(t3);
+            arqCategorias.update(t3);
 
-            t = arqTarefas.read(3);
+            t = arqCategorias.read(3);
             if (t != null) {
                 System.out.println("Tarefa encontrada: " + t + "\n");
             } else {
                 System.out.println("\nTarefa não encontrada!");
             }
 
-
-
-            arqTarefas.close();
+            arqCategorias.close();
 
         } catch (Exception e) {
             e.printStackTrace();
